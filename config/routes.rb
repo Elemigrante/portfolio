0922d90a17cb49075a9c36498c05d8249167a6d2
Elemigrante <gasdom@yandex.ru>
2019-08-20 11:26:23 +0300

    Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   resources :portfolios # from resource generator
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  
+  # get 'any string', to: 'controller#action'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Root path to home(view) page on localhost:3000 instead rails standard template.
+  # root to: 'controller#action', localhost:3000 now shows pages#home instead rails standard template.
+  root to: 'pages#home'
 end
