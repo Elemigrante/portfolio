1c4930017a380e7def8f9a1d4a9c28f1fd1de0ab
Elemigrante <gasdom@yandex.ru>
2019-08-21 13:14:17 +0300

    Added custom action for updating status

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,31 +1,31 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  
   # GET /blogs
   # GET /blogs.json
   def index
     @blogs = Blog.all
   end
-
+  
   # GET /blogs/1
   # GET /blogs/1.json
   def show
   end
-
+  
   # GET /blogs/new
   def new
     @blog = Blog.new
   end
-
+  
   # GET /blogs/1/edit
   def edit
   end
-
+  
   # POST /blogs
   # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
-
+    
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
@@ -34,7 +34,7 @@ class BlogsController < ApplicationController
       end
     end
   end
-
+  
   # PATCH/PUT /blogs/1
   # PATCH/PUT /blogs/1.json
   def update
@@ -43,11 +43,11 @@ class BlogsController < ApplicationController
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
       else
         format.html { render :edit }
-        
+      
       end
     end
   end
-
+  
   # DELETE /blogs/1
   # DELETE /blogs/1.json
   def destroy
@@ -56,15 +56,26 @@ class BlogsController < ApplicationController
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
     end
   end
-
+  
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+    
+    redirect_to blogs_url, notice: 'Post status has been updated'
+  end
+  
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.friendly.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def blog_params
-      params.require(:blog).permit(:title, :body)
-    end
+  
+  # Use callbacks to share common setup or constraints between actions.
+  def set_blog
+    @blog = Blog.friendly.find(params[:id])
+  end
+  
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def blog_params
+    params.require(:blog).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   # resource generator created 'resources :portfolios', but we override it for our purposes.
   resources :portfolios, except: [:show]
+  # custom route name
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
-  # get 'any string', to: 'controller#action'
+  # get 'any string', to: 'controller#action', hard coded route
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   
-  resources :blogs
+  # basic default resources or scaffold, resources generators
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   
   # Root path to home(view) page on localhost:3000 instead rails standard template.
   # root to: 'controller#action', localhost:3000 now shows pages#home instead rails standard template.
