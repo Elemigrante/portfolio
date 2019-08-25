5cf9b8161256a1f90a74092e3572d7315e175778
Elemigrante <gasdom@yandex.ru>
2019-08-25 14:49:44 +0300

    Implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
+  
   validates :title, :body, :main_image, :thumb_image, presence: true
   # two best practice to create custom scopes to call in controller
   # 1.
@@ -12,7 +14,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
   
   def set_defaults
-    self.main_image ||= "https://via.placeholder.com/600x400"
-    self.thumb_image ||= "https://via.placeholder.com/350x200"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
