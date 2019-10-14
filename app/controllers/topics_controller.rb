84d288924386e46a80c292c98ddf697de17a1135
Elemigrante <gasdom@yandex.ru>
2019-10-14 15:47:06 +0300

    Implemented delete button for topics if user site admin

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,5 @@
 class TopicsController < ApplicationController
+  before_action :set_topic, only: [:show, :destroy]
   before_action :set_sidebar_topics
   layout 'blog'
   
@@ -7,12 +8,21 @@ class TopicsController < ApplicationController
   end
 
   def show
-    @topic = Topic.find(params[:id])
-    
     if logged_in?(:site_admin)
       @blogs = @topic.blogs.recent.page(params[:page]).per(5)
     else
       @blogs = @topic.blogs.published.recent.page(params[:page]).per(5)
     end
+  end
+  
+  def destroy
+    @topic.destroy
+    respond_to do |format|
+      format.html { redirect_to topics_url, notice: 'Topic was successfully deleted.' }
+    end
+  end
+
+  def set_topic
+    @topic = Topic.find(params[:id])
   end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ApplicationRecord
   validates :title, presence: true
   
-  has_many :blogs
+  has_many :blogs, dependent: :destroy
   
   def self.with_blogs
     includes(:blogs).where.not(blogs: { id: nil })
